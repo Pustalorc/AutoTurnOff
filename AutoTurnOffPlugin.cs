d9fe5f123344e4155e7873225ba9ac8722291d61
Pustalorc <persiafighter@gmail.com>
2021-04-10 16:44:08 +0200

    Fixed no owner check

--- AutoTurnOffPlugin.cs
+++ AutoTurnOffPlugin.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Linq;
+using JetBrains.Annotations;
 using Pustalorc.Plugins.AutoTurnOff.Configuration;
-using Rocket.Core.Logging;
 using Rocket.Core.Plugins;
 using Rocket.Unturned;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
+using Logger = Rocket.Core.Logging.Logger;
 
 namespace Pustalorc.Plugins.AutoTurnOff
 {
+    // This references the original objects. Changes done to them should be reflected in the property accessors here.
     public class AutoTurnOffPlugin : RocketPlugin<AutoTurnOffConfiguration>
     {
         protected override void Load()
@@ -23,23 +25,31 @@ namespace Pustalorc.Plugins.AutoTurnOff
             Logger.Log("Auto Turn Off has been unloaded!");
         }
 
-        private void Disconnected(UnturnedPlayer player)
+        private void Disconnected([NotNull] UnturnedPlayer player)
         {
             var playerId = player.CSteamID.m_SteamID;
 
             if (BarricadeManager.regions == null)
                 return;
 
-            var buildables = BarricadeManager.regions.Cast<BarricadeRegion>().Concat(BarricadeManager.vehicleRegions).SelectMany(brd => brd.drops).ToList().Where(k => Configuration.Instance.ItemsToDisable.Contains(k.asset.id)).ToList();
-
-            foreach (BarricadeDrop build in buildables)
+            var regions = BarricadeManager.regions.Cast<BarricadeRegion>().Concat(BarricadeManager.vehicleRegions)
+                .ToList();
+            var drops = regions.SelectMany(k => k.drops).ToList();
+            var data = regions.SelectMany(k => k.barricades).ToList();
+            var buildables = data.Select((k, i) =>
             {
-                var interactable = build.interactable;
-                var model = build.model;
+                if (!Configuration.Instance.ItemsToDisable.Contains(k.barricade.asset.id))
+                    return null;
 
-                if (!BarricadeManager.tryGetInfo(interactable.transform, out var x, out var y, out var plant, out var index, out var region))
-                    continue;
+                if (k.owner != playerId)
+                    return null;
 
+                var drop = drops.ElementAt(i);
+                return drop == null ? null : new Buildable(k, drop);
+            }).Where(k => k != null).ToList();
+
+            foreach (var interactable in buildables.Select(build => build.Interactable))
+            {
                 switch (interactable)
                 {
                     case InteractableSafezone saf:
--- Configuration/AutoTurnOffConfiguration.cs
+++ Configuration/AutoTurnOffConfiguration.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using Rocket.API;
 
 namespace Pustalorc.Plugins.AutoTurnOff.Configuration
@@ -13,7 +11,7 @@ namespace Pustalorc.Plugins.AutoTurnOff.Configuration
         {
             ItemsToDisable = new List<ushort>
             {
-                458, 1230, 362, 459, 1050, 1466, 1250, 1261, 359, 360, 361, 1049, 1222, 
+                458, 1230, 362, 459, 1050, 1466, 1250, 1261, 359, 360, 361, 1049, 1222
             };
         }
     }
